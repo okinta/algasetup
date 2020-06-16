86244519df8ce44a2966b04ac04f04db37159d57
Robert Roose <robert@shivercube.com>
2020-06-16 14:54:21 +0100

    Load Vultr API key from vault

--- files/usr/local/bin/entrypoint.sh
+++ files/usr/local/bin/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+PLATFORM_API_KEY
+PLATFORM_API_KEY=$(wget -q -O - http://vault.in.okinta.ge:7020/api/kv/vultr_api_key)
+if [ -z "$PLATFORM_API_KEY" ]; then
+    echo "Could not obtain Vultr API key from Vault" >&2
+    exit 1
+fi
+export PLATFORM_API_KEY
+
 mkdir /infra
 wget -O /infra/alga.yaml \
     https://raw.githubusercontent.com/okinta/alga-infra/master/infra/alga.yaml
